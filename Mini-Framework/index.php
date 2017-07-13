f1deaed193e3ebce99278852311c0eff4a6cf006
xoch06 <xochitlmenendez@gmail.com>
2017-07-12 20:26:06 -0500

    Arreglos

--- Mini-Framework/index.php
+++ Mini-Framework/index.php
@@ -1,40 +1,32 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
- <title>Mini Framework PHP</title>
- <style>
- header{
- background: #e3e3e3;
- padding: 2em;
- text-align:  center;
- } 
- 	 </style>
- </head>
- <body>
- <header>
- 	<?php
- 	//variable super global GET
- 	// http: mini-framework.com/saludo=Hola&nombre=xochitl&edad=27 
- 	//este es un cometario
- 	$saludo = $_GET['saludo'];
- 	$nombre = $_GET['nombre'];
- 	$edad = $_GET['edad'];
 
- 	?>
- 	<h1> 
  	<?php
+ 	//Arreglos
+		$nombres = array(
+			'Alejandro',
+			'Luis',
+			'David' );
+
+		$animales = [
+			'Perro',
+			'Gato',
+			'Conejo' 
+			];
+
+			// Agregar elmentos a los  arreglos
+			array_push($nombres,'Pedro' );
+
+			// Traer un elemento en especifico
+			$dato = $animales[1];
+
+			//existe un elemento?
+			$respuesta = in_array("Alejandro",$nombres);
+			
+			//Eliminar un elemento
+			unset ($nombres[1]);
+
+
+
+
+ require 'index.view.php';
+
  	
- 	//echo 'Este es un saludo: $saludo' no imprime el valor del saludo
-
- 	// http://www.mini-framework.com/?nombre=Ale&saludo=Hola&edad=20
- 	 
- 	 echo $saludo . " " . $nombre; ?> 
- 	 <br>
- 	 <?php echo $edad; ?>
- 		
- 	</h1>
- </header>
- 
- </body>
- </html>
--- Mini-Framework/index.view.php
+++ Mini-Framework/index.view.php
@@ -6,24 +6,47 @@
  <style>
  header{
  background: #e3e3e3;
- padding: 2em;
+ padding-left: 2em;
  text-align:  center;
  } 
- 	 </style>
+ 
+     </style>
+ }
  </head>
  <body>
+ 
  <header>
- 	
- 	<h1> 
- 	<?php
- 	
-
- 	 echo $saludo . " " . $nombre; ?> 
- 	 <br>
- 	 <?php echo $edad; ?>
- 		
- 	</h1>
+ 
+ <h1>Arreglos</h1>
+ 
  </header>
+    
+    <main>
+ 
+<h2> Nombres: </h2>
+<ul>    
+    <?php foreach($nombres as $nombre){ ?>
+    <li> <?= $nombre ?> </li>
+    <?php } ?>
+        </ul>
+        
+        <h2> Animales </h2>
+        
+        <ol>    
+    <?php foreach ($animales as $animal): ?>
+    <li> <?= $animal ?> </li>
+    <?php endforeach;?>
+        </ol>
+        <p><? $dato; ?> </p>
+        <p> ¿Existe Alejandro? <? = $respuesta; ?> </p>
+
+        </main>
+    
+    <footer>
+        
+    </footer>
+        
+    
  
  </body>
  </html>

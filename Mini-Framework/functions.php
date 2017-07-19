d01b719120898aeda920172046ca7913562261da
xoch06 <xochitlmenendez@gmail.com>
2017-07-18 20:26:58 -0500

    coneccion de php a bd

--- Mini-Framework/functions.php
+++ Mini-Framework/functions.php
@@ -1,4 +1,20 @@
 <?php
+ 
+//conecta a la db
+function connectDB()
+ {
+	try{
+
+	$pdo = new PDO ('mysql:host=127.0.0.1;dbname=tareas','root','');
+	return $pdo;
+
+} catch (PDOException $ex) {
+	dd($e->getMessage());
+	}
+ }
+
+
+//para la funcion 
  function dd ($arreglo)
  {
  	echo '<pre>';
--- Mini-Framework/index.php
+++ Mini-Framework/index.php
@@ -3,6 +3,26 @@
  require 'functions.php';
  
 
+ $pdo = connectDB();
+
+// Se prepara la sentencia para su ejecucion
+$sentencia = $pdo->prepare('SELECT * FROM tarea');
+
+//se ejecuta la sentencia preparada
+$sentencia-> execute();
+// guarda los datos recuperados
+
+$tareas = $sentencia-> fetchAll();
+
+echo $tareas [0] ['descripcion'];
+echo $tareas [1] ['descripcion'];
+
+dd($tareas);
+
+
+
+ 
+
  require 'index.view.php';
 
  	
